50116bd0bed3369cb1ef00bb5186026ce1a4cb14
Kevin Suttle <kevin@kevinsuttle.com>
2013-04-22 23:06:54 -0400

    Add missing comma in spec_helper.rb
    Hopefully fixing the Travis build

--- spec/classes/kindle_spec.rb
+++ spec/classes/kindle_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'kindle' do
   it do
     should contain_package('Kindle').with({
-      :source   => 'http://kindleformac.amazon.com/40381/KindleForMac.dmg'
+      :source   => 'http://kindleformac.amazon.com/40381/KindleForMac.dmg', 
       :provider => 'appdmg'
     })
   end
